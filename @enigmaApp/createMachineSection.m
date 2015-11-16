cdb1a4ce546b318575788f063485114abf9effc5
eigsmw@mathworks <muthu.annamalai@mathworks.com>
2015-11-16 17:01:16 -0500

    minor update to eliminate pwd()

--- @enigmaApp/createMachineSection.m
+++ @enigmaApp/createMachineSection.m
@@ -6,8 +6,9 @@ function createMachineSection(app)
 % Copyright 2015, The MathWorks Inc.
 
 % Read in Enigma Machine background
-[~,filebase,ext]=fileparts(app.machineImageFile)
-enigmaBG.Image          = imread(fullfile(pwd(),'@enigmaApp','images',[filebase,ext]));
+[~,filebase,ext]=fileparts(app.machineImageFile);
+[basedir,~,~]=fileparts(mfilename('fullpath'));
+enigmaBG.Image          = imread(fullfile(basedir,'images',[filebase,ext]));
 enigmaBG.Height         = size(enigmaBG.Image,1);
 enigmaBG.Width          = size(enigmaBG.Image,2);
 enigmaBG.AspectRatioW2H = enigmaBG.Width/enigmaBG.Height;
